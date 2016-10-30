6497f3df92a4dd78f79281824543adc2aa17b001
Jose Antonio Sanchez <jasancheg@gmail.com>
2016-10-30 16:11:01 -0600

    some extras

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,14 +2,16 @@ import { FETCH_WEATHER } from '../actions/index';
 
 export default function(state = [], action) {
 
+  console.log('action received', action);
   // never do state = something
   // Avoid mutate the state by do push
   // or any operation over the same state
   // concat is a good solution for it and
   // return a complete new value
-  switch(type) {
+  switch(action.type) {
     case FETCH_WEATHER:
-      return [action.payload.data, ...state];
+      return [ action.payload.data, ...state ];
   }
+
   return state;
 }
